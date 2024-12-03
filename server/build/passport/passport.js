dd0cfe30d9aa2bf5d93f8dfa29c385d12840e4b4
Eliot Nagy <nagyeliot@gmail.com>
2024-12-03 12:44:57 +0100

    some fixes

--- server/build/passport/passport.js
+++ server/build/passport/passport.js
@@ -21,7 +21,7 @@ const configurePassport = (passport) => {
                         done('Incorrect username or password.');
                     }
                     else {
-                        done(null, user._id);
+                        done(null, user);
                     }
                 });
             }
