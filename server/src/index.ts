c9668ab1073ea7603ada743d2898be5b3abdb09e
Eliot Nagy <nagyeliot@gmail.com>
2024-12-04 19:06:58 +0100

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,14 +32,14 @@ const corsOptions = {
     credentials: true
 };
 
-app.use(cors(corsOptions));
-
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET ,HEAD, OPTIONS, POST, PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
